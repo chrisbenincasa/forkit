16151e892d2331ed6c55ed300213b377eed97946
Christian Benincasa <chrisbenincasa@gmail.com>
2012-05-29 15:53:10 -0400

    difficulty fixes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe < ActiveRecord::Base
   has_many :users, :through => :personalRecipeInfo
 
   validates_uniqueness_of :name
-  validates_inclusion_of :difficulty, :in => %w(Beginner Moderate Difficult Expert)
+  #validates_inclusion_of :difficulty, :in => %w(Beginner Moderate Difficult Expert)
 
   def to_param
     name.parameterize
