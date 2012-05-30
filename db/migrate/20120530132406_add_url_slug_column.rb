c569278b68db4115126af35b1a4846ba12a9fe9c
Christian Benincasa <chrisbenincasa@gmail.com>
2012-05-30 16:38:14 -0400

    fix migrations

--- db/migrate/20120530132406_add_url_slug_column.rb
+++ db/migrate/20120530132406_add_url_slug_column.rb
@@ -1,6 +1,5 @@
 class AddUrlSlugColumn < ActiveRecord::Migration
   def change
     add_column :recipes, :url_slug, :string, {:null => false, :default => ''}
-    remove_column :recipes, :difficulty
   end
 end
